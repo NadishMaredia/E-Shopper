40cc30bcc62ac6b5faa3d3134fdda6a45496fd1d
Nadish Maredia <nadishns97@gmail.com>
2024-04-22 11:32:13 -0400

    Edit and Delete Product

--- EShop.Web/Controllers/ProductController.cs
+++ EShop.Web/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using EShop.Web.Services.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace EShop.Web.Controllers
 {
@@ -32,5 +33,86 @@ namespace EShop.Web.Controllers
 
 			return View(productList);
 		}
+
+		public async Task<IActionResult> ProductCreate()
+		{
+			return View();
+		}
+		[HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductDto productDto)
+        {
+			if(ModelState.IsValid)
+			{
+				ResponseDto? response = await productService.CreateProduct(productDto);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Product created successfully";
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
+            }
+            return View(productDto);
+        }
+
+		[HttpGet]
+		public async Task<IActionResult> ProductDelete(int productId)
+		{
+			if (ModelState.IsValid)
+			{
+				ResponseDto? response = await productService.DeleteProductById(productId);
+
+				if (response != null && response.IsSuccess)
+				{
+					TempData["success"] = "Product deleted successfully";
+				}
+				else
+				{
+					TempData["error"] = response?.Message;
+				}
+			}
+			return RedirectToAction(nameof(ProductIndex));
+		}
+
+		public async Task<IActionResult> ProductEdit(int productId)
+		{
+			ResponseDto? response = await productService.GetProductByIdAsync(productId);
+
+			if(response != null && response.IsSuccess)
+			{
+				ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+				return View(model);
+			}
+			else
+			{
+                TempData["error"] = response?.Message;
+            }
+			return NotFound();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ProductEdit(ProductDto productDto)
+		{
+            if (ModelState.IsValid)
+            {
+				ResponseDto? response = await productService.UpdateProduct(productDto);
+
+				if(response != null && response.IsSuccess)
+				{
+                    TempData["success"] = "Product updated successfully";
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
+            }
+
+            return View(productDto);
+        }
+
 	}
 }
