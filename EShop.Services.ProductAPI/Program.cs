40f0d93fd28076f2eeec8b07ee3e1e6b01929c16
Nadish Maredia <nadishns97@gmail.com>
2024-04-21 21:42:06 -0400

    Add Migration SeedDataToProducts

--- EShop.Services.ProductAPI/Program.cs
+++ EShop.Services.ProductAPI/Program.cs
@@ -1,6 +1,14 @@
+using EShop.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +31,16 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void ApplyMigrations()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+		if (_db.Database.GetPendingMigrations().Count() > 0)
+		{
+			_db.Database.Migrate();
+		}
+	}
+}
